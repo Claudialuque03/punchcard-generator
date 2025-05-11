765ff8464ee5eb47e2bd4f45313cfe7ca31ff861
Claudialuque03 <claudia.luque@student.bau.cat>
2025-05-11 20:44:58 +0200

    Update main.js

--- main.js
+++ main.js
@@ -87,10 +87,10 @@ function generateSVG(pattern, machineStitches = 24) {
     const FIXED_HOLE_DIAMETER = 3;
 
     // Configuración
-    const SPACING = 5.43;              // Entre agujeros del patrón
-    const SPACING_FIXED_HOLE = 12.822;   // Entre patrón y fijos
-    const MARGIN = 12.17;                // Margen externo
-    const ROW_SPACING = 5.02;            // Entre filas
+    const SPACING = 4.55;              // Entre agujeros del patrón
+    const SPACING_FIXED_HOLE = 5.55;   // Entre patrón y fijos
+    const MARGIN = 13;                // Margen externo
+    const ROW_SPACING = 5;            // Entre filas
 
     const numCols = machineStitches;
     const numRows = pattern.length;
